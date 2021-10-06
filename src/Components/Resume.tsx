fa58b23983e0e5ad3da7430a1a35cdc235f9c4d1
Verena Traub <verena@yngage.de>
2021-10-06 14:18:32 +0200

    adds styling to CV page

--- src/Components/Resume.tsx
+++ src/Components/Resume.tsx
@@ -2,18 +2,22 @@ import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 
 import { makeStyles } from '@mui/styles';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Grid, IconButton } from '@mui/material';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 import testFile from '../assets/test.pdf';
 import Navbar from './Navbar';
 
 const useStyles = makeStyles({
   mainContainer: {
-    margin: '6rem auto'
-  },
-  documentView: {
+    marginTop: '5rem',
     display: 'flex',
     justifyContent: 'center'
+  },
+  pageContainer: {
+    textAlign: 'center',
+    marginBottom: '0.5rem'
   }
 });
 
@@ -41,24 +45,36 @@ const Resume = () => {
   return (
     <Box className={classes.mainContainer}>
       <Navbar />
-      <Document file={testFile} onLoadSuccess={onDocumentLoaded}>
-        <Page pageNumber={pageNumber} className={classes.documentView} />
-      </Document>
-      <div>
-        <Typography variant="body1" style={{ color: '#c2c4c2' }}>
-          Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-        </Typography>
-        <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
-          Previous
-        </button>
-        <button
-          type="button"
-          disabled={pageNumber >= numPages}
-          onClick={nextPage}
+      <Box>
+        <Box className={classes.pageContainer}>
+          <Typography variant="body1" style={{ color: '#c2c4c2' }}>
+            Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+          </Typography>
+        </Box>
+        <Grid
+          container
+          spacing={3}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
         >
-          Next
-        </button>
-      </div>
+          <Grid item>
+            <IconButton onClick={previousPage} disabled={pageNumber <= 1}>
+              <ArrowBackIosIcon style={{ color: '#c2c4c2' }} />
+            </IconButton>
+          </Grid>
+          <Grid item>
+            <Document file={testFile} onLoadSuccess={onDocumentLoaded}>
+              <Page pageNumber={pageNumber} />
+            </Document>
+          </Grid>
+          <Grid item>
+            <IconButton onClick={nextPage} disabled={pageNumber >= numPages}>
+              <ArrowForwardIosIcon style={{ color: '#c2c4c2' }} />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 };
